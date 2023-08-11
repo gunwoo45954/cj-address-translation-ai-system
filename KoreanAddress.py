6f1cbb64bb528797dd36ed72ea8c2defd2cbfb27
JunHyung1206 <pjh120610@gmail.com>
2023-08-11 19:03:01 +0900

    다중 주소결과 처리 수정

--- KoreanAddress.py
+++ KoreanAddress.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,11 +20,22 @@ def get_address(api_key, word):
     # 결과는 여러 개가 나오는데 도로명주소는 모두 동일한 경우.
     # 결과는 여러 개가 나오는데 검색어와 1번째 결과가 일치하는 경우. 나머지 결과들은 다름.
     # 문제점 : 검색어와 1번째 결과가 조금 달라서 "답 없음"처리가 되는 경우. 이 경우에는 1번째 결과가 정답인 경우임.
-    if (len(data) > 1) and (data[0] != data[1]):
-        index = data[0].find("(")
-        if (word == data[0]) or (word == data[0][:index-1]):    # 검색어가 ()안에 있는 문자를 삭제했을 때와 동일하거나, 삭제하지 않았을 때와 동일하거나
-            pass
-        else:
-            data = ["답 없음"]
+    if (len(data) > 1):
 
+        # data[1]에서 ()를 없앤 문자열이매칭되는지 검색 -> 매칭되면 data[0] 사용 아니라면 답 없음
+        
+        # 되는 경우
+        # data[0] = 서울특별시 서대문구 통일로 81 (미근동) 
+        # data[1] = 서울특별시 서대문구 통일로 83-1 (미근동) 인경우
+        # 서울특별시 서대문구 통일로 81
+        
+        # 안되는 경우
+        # data[0] = 서울특별시 서대문구 통일로 81 (미근동) 
+        # data[1] = 서울특별시 서대문구 통일로 87 (미근동) 인경우
+        # 서울특별시 서대문구 통일로 81가 매칭되지 않음 -> 답없음
+        
+        temp = re.sub(r'\([가-힣]+\)','',data[0]).strip()
+        m = re.match(temp,data[1])
+        if m is None:
+            data = ["답 없음"]
     return data[0]
